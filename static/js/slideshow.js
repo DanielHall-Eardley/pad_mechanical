1e116f488bbb945b77408a465c4db9fb7be5f136
DanielHall-Eardley <350chevy8@gmail.com>
2023-12-11 16:13:55 -0500

    pic slideshow

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,22 +1,60 @@
-function scrollSlideShow(currentSlide, slideshow) {
-  const scrollLength = currentSlide * 40;
-  slideshow.style.transform = `translate(-${scrollLength}vw, 0)`;
-}
-
-const slideshows = Array.from(document.getElementsByClassName("slideshow"));
-console.log(slideshows);
-slideshows.forEach((slideshow) => {
+function scrollSlideShow(slideNumber, slideshow, direction, title) {
   const slides = slideshow.children;
   const last = slides.length - 1;
+
+  if (slideNumber < last && direction === "next") {
+    const newSlideNumber = slideNumber + 1;
+    const scrollLength = `translate(-${newSlideNumber * 40}vw, 0)`;
+    slideshow.style.transform = scrollLength;
+    updateSlideName(newSlideNumber, slides, title);
+    return newSlideNumber;
+  }
+
+  if (slideNumber >= last && direction === "next") {
+    const newSlideNumber = 0;
+    slideshow.style.transform = `translate(0, 0)`;
+    updateSlideName(newSlideNumber, slides, title);
+    return newSlideNumber;
+  }
+
+  if (slideNumber > 0 && direction === "prev") {
+    const newSlideNumber = slideNumber - 1;
+    const scrollLength = `translate(-${newSlideNumber * 40}vw, 0)`;
+    slideshow.style.transform = scrollLength;
+    updateSlideName(newSlideNumber, slides, title);
+    return newSlideNumber;
+  }
+
+  return 0;
+}
+
+function updateSlideName(slideNumber, slides, title) {
+  const slideNameArray = slides[slideNumber].src.split("/");
+  const slideName = slideNameArray[slideNameArray.length - 1];
+  title.innerText = slideName;
+}
+
+const containers = Array.from(
+  document.getElementsByClassName("slideshow-container")
+);
+
+containers.forEach((container) => {
+  const slideshow = container.querySelector(".slideshow");
+  const prevBtn = container.querySelector(".prev");
+  const nextBtn = container.querySelector(".next");
+  const title = container.querySelector(".picture-title");
   let current = 0;
-
   const intervalId = setInterval(() => {
-    if (current < last) {
-      current += 1;
-    } else {
-      current = 0;
-    }
-
-    scrollSlideShow(current, slideshow);
+    current = scrollSlideShow(current, slideshow, "next", title);
   }, 10000);
+
+  prevBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    current = scrollSlideShow(current, slideshow, "prev", title);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    current = scrollSlideShow(current, slideshow, "next", title);
+  });
 });
